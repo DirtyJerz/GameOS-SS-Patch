35f2ea748fceaeb40e02cb844a2c525ffab7ffe8
DirtyJerz <fluidflow00@gmail.com>
2011-11-08 10:46:35 -0500

    initial push

---
+++ patch_ss/source/main.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) DirtyJerz 2011 (word to ya motha)
+ * modified from git://git.gitbrew.org/ps3/otheros-utils/increase_bootmem_size.git
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <unistd.h>
+
+#include <psl1ght/lv2/net.h>
+
+#include <lv2_syscall.h>
+#include <udp_printf.h>
+
+/*
+ * GameOS system manager object in HV process 9
+ */
+#define GAMEOS_SYSMGR_OFFSET				0x0075bdd0ull /* 0x000a5dd0ull */
+
+/*
+ * 3.55 offsets
+ */
+
+#define DISPMGR_SET_LAID_OFFSET_355 			(GAMEOS_SYSMGR_OFFSET + 0x0016f3bcull)
+#define DISPMGR_SS_ID_OFFSET_355			(GAMEOS_SYSMGR_OFFSET + 0x0016f3e0ull)
+#define DISPMGR_SEND_SPM_REQ_OFFSET_355 		(GAMEOS_SYSMGR_OFFSET + 0x0016f458ull)
+
+#define DISPMGR_LAID_DISABLE				0x60000000ull
+#define DISPMGR_SS_ID_DISABLE				0x38600001ull
+#define DISPMGR_SPM_DISABLE1				0x3be00001ull
+#define DISPMGR_SPM_DISABLE2				0x9be10070ull
+#define DISPMGR_SPM_DISABLE3				0x38600000ull
+
+/*
+ * main
+ */
+int main(int argc, char **argv)
+{
+	netInitialize();
+
+	udp_printf_init();
+
+	PRINTF("%s:%d: start\n", __func__, __LINE__);
+
+	PRINTF("%s:%d: patching overwriting of LAID\n", __func__, __LINE__);
+
+	lv2_lv1_poke(DISPMGR_SET_LAID_OFFSET_355, DISPMGR_LAID_DISABLE);
+
+	PRINTF("%s:%d: end\n", __func__, __LINE__);
+
+
+
+	PRINTF("%s:%d: disabling SS ID check\n", __func__, __LINE__);
+
+        lv2_lv1_poke(DISPMGR_SS_ID_OFFSET_355, DISPMGR_SS_ID_DISABLE);
+
+        PRINTF("%s:%d: end\n", __func__, __LINE__);
+
+
+
+	PRINTF("%s:%d: disabling SPM\n", __func__, __LINE__);
+
+        lv2_lv1_poke(DISPMGR_SEND_SPM_REQ_OFFSET_355, DISPMGR_SPM_DISABLE1);
+	lv2_lv1_poke(DISPMGR_SEND_SPM_REQ_OFFSET_355 + 4, DISPMGR_SPM_DISABLE2);
+	lv2_lv1_poke(DISPMGR_SEND_SPM_REQ_OFFSET_355 + 8, DISPMGR_SPM_DISABLE3);
+
+        PRINTF("%s:%d: end\n", __func__, __LINE__);
+
+	
+	udp_printf_deinit();
+
+	netDeinitialize();
+
+	return 0;
+}
